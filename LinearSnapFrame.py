341078705ed3ab7eda98554ecfcdd5d5b4e452a1
Colin McFadden <colin@hedge.video>
2025-04-01 11:08:44 -0500

    fix width

--- LinearSnapFrame.py
+++ LinearSnapFrame.py
@@ -17,7 +17,7 @@ import wx.xrc
 class LinearSnapFrame ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 1366,710 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 1200,710 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
